59b015a5451e7afa728b7384d4bf579b1722fb06
manav mistry <mistrymanav2662@gmail.com>
2026-09-08 20:33:39 +0530

    stage 2 done

--- task1/code/calculations.js
+++ task1/code/calculations.js
@@ -4,7 +4,7 @@ function calculateSubtotal(order){
     let subtotal=0;
     for(const item of order.items){
         for(const product of products){
-        if (item.productId === product.id) {
+        if (item.productId == product.id) {
                 subtotal += product.price * item.quantity;
 
              }
@@ -59,10 +59,95 @@ function getOrdersByCustomer(customerId){
     return customerorders;
 
 }
-for (const order of orders){
-console.log(generateOrderSummary(order));
-}
-orders.forEach(order => {
-    console.log(generateOrderSummary(order));
+// for (const order of orders){
+// console.log(generateOrderSummary(order));
+// }
+// orders.forEach(order => {
+//     console.log(generateOrderSummary(order));
     
+// });
+function getAllOrderTotals(){
+    const orderTotal=orders.map(order =>{
+        const subtotal= calculateSubtotal(order);
+        return calculateTotal(subtotal);
+    });
+    return orderTotal;
+}
+console.log(getAllOrderTotals());
+
+function calculateTotalRevenue(){
+    const orderTotal=getAllOrderTotals();
+    return orderTotal.reduce((sum,x)=>sum+x,0);
+}
+console.log(calculateTotalRevenue());
+
+function calculateAverageOrderValue(){
+    const total= calculateTotalRevenue();
+    const orderTotal=getAllOrderTotals();
+    const average=total/orderTotal.length;
+    return average;
+}
+console.log(calculateAverageOrderValue());
+
+function getHighestValueOrder(){
+   const highestOrder=orders.reduce((highest, order)=>
+{
+    const highestTotal= calculateTotal(calculateSubtotal(highest));
+    const orderTotal=calculateTotal(calculateSubtotal(order));
+    if(orderTotal>highestTotal){
+        return order;
+    }
+    else {
+        return highest;
+    }
 });
+return highestOrder;
+    
+}
+console.log(getHighestValueOrder());
+function hashighValueOrder(){
+   const highValueOrders = orders.filter(order => {
+    const total = calculateTotal(calculateSubtotal(order));
+    return total > calculateAverageOrderValue();
+    });
+    const orderId=highValueOrders.map(order=>order.id);
+    return orderId;
+}
+console.log(hashighValueOrder());
+
+function areAllOrdersValid(){
+    const validity=orders.every(order =>{
+
+    const validcustomer= customers.find(custs=>custs.id==order.customerId);
+    if(validcustomer==null){
+        return false;
+    }
+    const productValid=order.items.every(item=>products.find(p=>p.id==item.productId)
+    );
+    if(productValid==false){
+        return false;
+    }
+    const quantityValid=order.items.every(item=>
+        item.quantity>0
+    );
+    if(quantityValid==false){
+        return false;
+    }
+    return true;
+});
+return validity;
+
+}
+function updateOrder(order,updates){
+    const newOrder={...order,
+        ...updates
+    };
+   return newOrder;
+}
+const updated = updateOrder(orders[0], {
+    status: "failed",
+    paymentStatus:'failed',
+    tries:'2'
+});
+
+console.log(updated);
