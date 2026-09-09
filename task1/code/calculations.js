2a1afe4708ad6c502d1c78fbe2989fc7ae08bbce
manav mistry <mistrymanav2662@gmail.com>
2026-09-09 18:44:18 +0530

    stage 3 done

--- task1/code/calculations.js
+++ task1/code/calculations.js
@@ -73,13 +73,13 @@ function getAllOrderTotals(){
     });
     return orderTotal;
 }
-console.log(getAllOrderTotals());
+//console.log(getAllOrderTotals());
 
 function calculateTotalRevenue(){
     const orderTotal=getAllOrderTotals();
     return orderTotal.reduce((sum,x)=>sum+x,0);
 }
-console.log(calculateTotalRevenue());
+//console.log(calculateTotalRevenue());
 
 function calculateAverageOrderValue(){
     const total= calculateTotalRevenue();
@@ -87,7 +87,7 @@ function calculateAverageOrderValue(){
     const average=total/orderTotal.length;
     return average;
 }
-console.log(calculateAverageOrderValue());
+//console.log(calculateAverageOrderValue());
 
 function getHighestValueOrder(){
    const highestOrder=orders.reduce((highest, order)=>
@@ -104,7 +104,7 @@ function getHighestValueOrder(){
 return highestOrder;
     
 }
-console.log(getHighestValueOrder());
+//console.log(getHighestValueOrder());
 function hashighValueOrder(){
    const highValueOrders = orders.filter(order => {
     const total = calculateTotal(calculateSubtotal(order));
@@ -113,7 +113,7 @@ function hashighValueOrder(){
     const orderId=highValueOrders.map(order=>order.id);
     return orderId;
 }
-console.log(hashighValueOrder());
+//console.log(hashighValueOrder());
 
 function areAllOrdersValid(){
     const validity=orders.every(order =>{
@@ -150,4 +150,257 @@ const updated = updateOrder(orders[0], {
     tries:'2'
 });
 
-console.log(updated);
+//console.log(updated);
+
+function getCustomer(customerId)
+{
+    const promise= new Promise((resolve,reject)=>{
+
+            setTimeout(()=>{
+                    const customer= customers.find(cust=> cust.id==customerId);
+
+                    if(!customer){
+                        reject(new Error("not found"));
+                        return;
+                    }
+                    resolve(customer);
+            },1000);
+    });
+    return promise;
+}
+//with callback
+function getCustomerc(customerId,callback){
+    setTimeout(()=>{
+        const customer= customers.find((cust)=>cust.id==customerId);
+        if(!customer){ 
+            callback(new Error("Customer not found"),null);
+        return;
+        }
+        callback(null,customer);
+    },1000)
+}
+// getCustomerc(2,(error,customer)=>{
+//     if (error) {
+//         console.log(error.message);
+//         return;
+//     }
+
+//     console.log(customer);
+// })
+// getCustomer(1)
+//     .then(customer => {
+//         console.log(customer);
+//     })
+//     .catch(error => {
+//         console.log(error.message);
+// });
+function getProducts(productId){
+    const promise= new Promise((resolve, reject)=>{
+        setTimeout(()=>{
+            const product=products.find((pro)=>pro.id==productId);
+
+            if(!product){
+                reject(new Error("product not found"));
+            return;
+            }
+            resolve(product);
+        },1500)
+    })
+    return promise;
+
+}
+function getProductsc(productId,callback){
+    setTimeout(()=>{
+             const product=products.find((pro)=>pro.id==productId);
+        if(!product){
+                callback(new Error("product not found"),null)
+                return;
+            }
+
+        callback(null,product)
+
+    },1000)
+}
+// getProducts(101)
+//     .then(product => {
+//         console.log(product);
+//     })
+//     .catch(error => {
+//         console.log(error.message);
+// });
+// getProductsc(101, (error, product) => {
+//     if(error){
+//         console.log(error.message);
+//         return;
+//     }
+
+//     console.log(product);
+// });
+function checkStockc(items, callback) {
+    setTimeout(() => {
+
+        for (const item of items) {
+            
+
+            const prod = products.find(
+                prod => prod.id == item.productId
+            );
+
+            if (!prod) {
+                callback(new Error("Product not found"), null);
+                return;
+            }
+
+            if (prod.stock < item.quantity) {
+                callback(new Error("Not enough stock"), null);
+                return;
+            }
+        }
+
+        callback(null, true);
+
+    }, 1000);
+}
+function checkStock(items){
+    const promise=new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+                for(const item of items){
+                 
+                    const product =products.find((prod)=>prod.id==item.productId)
+                    if(!product){
+                        reject(new Error("product not found"));
+                        return
+                    }
+                    if(product.stock<item.quantity){
+                        reject(new Error("not enough stock"));
+                        return
+                    }
+                }
+                resolve(true)
+
+        },1000)
+    })
+    return promise
+}
+// const order = orders.find(order => order.id === 1001);
+
+// checkStockc(order.items, (error, result) => {
+
+//     if (error) {
+//         console.log("ERROR:", error.message);
+//         return;
+//     }
+
+//     console.log("SUCCESS:", result);
+// });
+
+function processPayment(order){
+    const promise = new Promise((resolve,reject)=>
+    {
+            setTimeout(()=>{
+
+                if(order.paymentStatus==='paid'){
+                    resolve(order)
+                }
+                else{
+                    reject(new Error("payment not succesful"))
+                }
+
+            },1000)
+    });
+    return promise;
+}
+function processPaymentc(order,callback){
+    setTimeout(()=>{
+        if(order.paymentStatus==='paid'){
+            callback(null,order)
+            return
+        }
+        callback(new Error("not succesful"),null)
+
+    },1000)
+}
+// processPayment(orders[0])
+//     .then(order => {
+//         console.log("Payment successful:", order);
+//     })
+//     .catch(error => {
+//         console.log("Payment failed:", error.message);
+// });
+function createordercall(order,callback){
+    setTimeout(()=>{
+
+
+        callback(null,order)
+    },1000)
+}
+const order = orders.find(order => order.id === 1002);
+
+getCustomerc(order.customerId, (error, customer) => {
+
+    if (error) {
+        console.log(error.message);
+        return;
+    }
+
+    console.log("Customer found:", customer);
+
+    let completed = 0;
+
+    for (const item of order.items) {
+
+        getProductsc(item.productId, (error, product) => {
+
+            if (error) {
+                console.log(error.message);
+                return;
+            }
+
+            console.log("Product found:", product);
+
+            completed++;
+
+            if (completed === order.items.length) {
+
+                checkStockc(order.items, (error, stock) => {
+
+                    if (error) {
+                        console.log(error.message);
+                        return;
+                    }
+
+                    console.log("Stock available:", stock);
+
+                    processPaymentc(order, (error, payment) => {
+
+                        if (error) {
+                            console.log(error.message);
+                            return;
+                        }
+
+                        console.log("Payment successful:", payment);
+
+                        createordercall(order, (error, createdOrder) => {
+
+                            if (error) {
+                                console.log(error.message);
+                                return;
+                            }
+
+                            console.log("Order created:", createdOrder);
+                        });
+                    });
+                });
+            }
+        });
+    }
+});
+
+
+function createOrder(order){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            resolve(order)
+        },1000)
+    });
+}
